944d150c56f4a796f6f7208dfcaa138797fe318b
deepakroshan940-coder <deepakroshan940@gmail.com>
2025-09-17 20:26:04 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+        int a = 10, b = 20;
+        System.out.println("Sum = " + (a + b));
+    }
+}
